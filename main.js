5c8fe17b283f5c071919e082a4ae0ead946a0b29
khiem.pham <pk.22092000@gmail.com>
2024-10-16 02:37:29 +0700

    update right section header and implement carousel banner

---
+++ main.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log('Loaded!');
+
+  const carouselList = document.querySelector('.carousel-list');
+  const slides = document.querySelectorAll('.carousel-slide');
+  const prevBtn = document.querySelector('.carousel-control-btn.prev-btn');
+  const nextBtn = document.querySelector('.carousel-control-btn.next-btn');
+
+  let intervalId = null;
+  let currentIndex = 0;
+  const totalSlides = slides.length;
+
+  function updateCarousel() {
+    const offset = -currentIndex * 100; // Di chuyển carousel theo Slide
+    carouselList.style.transform = `translateX(${offset}%)`;
+
+    if (intervalId) {
+      // Reset chuyển Slide mỗi 5 giây
+      clearInterval(intervalId);
+    }
+
+    // Tự động chuyển Slide mỗi 5 giây
+    intervalId = setInterval(showNextSlide, 5000);
+  }
+
+  // Xử lý hiển thị Slide trước đó
+  function showPrevSlide() {
+    console.log('Prev Slide');
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    updateCarousel();
+  }
+
+  // Xử lý hiển thị Slide tiếp theo
+  function showNextSlide() {
+    console.log('Next Slide');
+    currentIndex = (currentIndex + 1) % totalSlides;
+    updateCarousel();
+  }
+
+  // Thêm event click cho 2 button
+  prevBtn.addEventListener('click', showPrevSlide);
+  nextBtn.addEventListener('click', showNextSlide);
+
+  updateCarousel();
+});
